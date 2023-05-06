079452b78c9bdf830e1848d03be16701be20c250
Abhijith KR <abhijithkr7745@gmail.com>
2023-05-06 23:10:39 +0530

    RichTextField_Editor => Quill [1.2_custom_quill_validation_using_inputbox] - Added

--- quill/urls.py
+++ quill/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     path('quill_validation_using_inputbox', quill_validation_using_inputbox, name='quill_validation_using_inputbox'),
     path('inline_quill_validation_using_inputbox', inline_quill_validation_using_inputbox, name='inline_quill_validation_using_inputbox'),
+    path('custom_quill_validation_using_inputbox', custom_quill_validation_using_inputbox, name='custom_quill_validation_using_inputbox'),
     
 ]
--- quill/views.py
+++ quill/views.py
@@ -28,3 +28,16 @@ def inline_quill_validation_using_inputbox(request):
 
 
     return render(request, 'quill/1.1_inline_quill_validation_using_inputbox..html')
+
+
+def custom_quill_validation_using_inputbox(request):
+
+    if request.method == 'POST':
+        data = request.POST.get('description')
+        print(data)
+
+        quill_data.objects.create(description=data)
+        print('custom_quill_validation_using_inputbox data saved successfully')
+
+
+    return render(request, 'quill/1.2_custom_quill_validation_using_inputbox.html')
