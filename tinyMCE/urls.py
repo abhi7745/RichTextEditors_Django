5367b0f3e9ff80ef9aa19fa819814ed503440527
Abhijith KR <abhijithkr7745@gmail.com>
2023-05-18 18:29:25 +0530

    RichTextField_Editor => tinyMCE [1.1_basic_tinyMCE_direct_validation_with_api_key] - Added

--- tinyMCE/urls.py
+++ tinyMCE/urls.py
@@ -5,4 +5,5 @@ from . views import *
 urlpatterns = [
 
     path('basic_tinyMCE_direct_validation_without_api_key', basic_tinyMCE_direct_validation_without_api_key, name='basic_tinyMCE_direct_validation_without_api_key'),
+    path('basic_tinyMCE_direct_validation_with_api_key', basic_tinyMCE_direct_validation_with_api_key, name='basic_tinyMCE_direct_validation_with_api_key'),
 ]
--- tinyMCE/views.py
+++ tinyMCE/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from . models import tinyMCE_data
 
+from .config import api_key
+
 # Create your views here.
 
 
@@ -15,3 +17,15 @@ def basic_tinyMCE_direct_validation_without_api_key(request):
         print('basic_tinyMCE_direct_validation_without_api_key data saved successfully')
 
     return render(request, 'tinyMCE/1.0_basic_tinyMCE_direct_validation_without_api_key.html')
+
+
+def basic_tinyMCE_direct_validation_with_api_key(request):
+
+    if request.method == 'POST':
+        data = request.POST.get('description')
+        print(data)
+
+        tinyMCE_data.objects.create(description=data)
+        print('basic_tinyMCE_direct_validation_with_api_key data saved successfully')
+
+    return render(request, 'tinyMCE/1.1_basic_tinyMCE_direct_validation_with_api_key.html', {'api_key': api_key})
