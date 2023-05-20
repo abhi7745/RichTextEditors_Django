f72e8be0fdab2dcbd21f1eabd0afc1ef9110517c
Abhijith KR <abhijithkr7745@gmail.com>
2023-05-20 19:00:33 +0530

    RichTextField_Editor => tinyMCE [1.3_distraction_free_tinyMCE_validation_using_inputbox_with_api_key] - Added

--- tinyMCE/urls.py
+++ tinyMCE/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('basic_tinyMCE_direct_validation_without_api_key', basic_tinyMCE_direct_validation_without_api_key, name='basic_tinyMCE_direct_validation_without_api_key'),
     path('basic_tinyMCE_direct_validation_with_api_key', basic_tinyMCE_direct_validation_with_api_key, name='basic_tinyMCE_direct_validation_with_api_key'),
     path('custom_basic_tinyMCE_direct_validation_with_api_key', custom_basic_tinyMCE_direct_validation_with_api_key, name='custom_basic_tinyMCE_direct_validation_with_api_key'),
+    path('distraction_free_tinyMCE_validation_using_inputbox_with_api_key', distraction_free_tinyMCE_validation_using_inputbox_with_api_key, name='distraction_free_tinyMCE_validation_using_inputbox_with_api_key'),
 ]
--- tinyMCE/views.py
+++ tinyMCE/views.py
@@ -40,3 +40,14 @@ def custom_basic_tinyMCE_direct_validation_with_api_key(request):
         print('custom_basic_tinyMCE_direct_validation_with_api_key data saved successfully')
 
     return render(request, 'tinyMCE/1.2_custom_basic_tinyMCE_direct_validation_with_api_key.html', {'api_key': api_key})
+
+def distraction_free_tinyMCE_validation_using_inputbox_with_api_key(request):
+
+    if request.method == 'POST':
+        data = request.POST.get('description')
+        print(data)
+
+        tinyMCE_data.objects.create(description=data)
+        print('distraction_free_tinyMCE_validation_using_inputbox_with_api_key data saved successfully')
+
+    return render(request, 'tinyMCE/1.3_distraction_free_tinyMCE_validation_using_inputbox_with_api_key.html', {'api_key': api_key})
